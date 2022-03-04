5c4b189a5a1dd15a86302b27f3648737398060b8
StellaMUjiu <100898242+StellaMUjiu@users.noreply.github.com>
2022-03-04 13:51:32 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '6cefaaa5bd2444dab05e1806cab0e95c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: '个人简历',
-  domain: 'transitivebullsh.it',
+  domain: 'https://material-yellowhorn-4b7.notion.site/6cefaaa5bd2444dab05e1806cab0e95c',
   author: 'Stella',
 
   // open graph metadata (optional)
